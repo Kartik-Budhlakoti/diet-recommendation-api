ac36e22122bf84e6c5e800961783748e631d72cc
Kartik Budhlakoti <professorkarma17@gmail.com>
2026-05-23 19:34:02 +0530

    add user registration endpoint

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,3 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app import schemas , models
+from app.database import get_db
 
 router = APIRouter()
+
+@router.post("/register" , response_model=schemas.UserResponse)
+def register_user(user: schemas.UserCreate, db : Session= Depends(get_db)):
+    db_user = models.User(
+        name=user.name,
+        age=user.age,
+        weight_kg= user.weight_kg,
+        height_cm=user.height_cm,
+        gender= user.gender,
+        activity_level=user.activity_level,
+        health_goal=user.health_goal
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
